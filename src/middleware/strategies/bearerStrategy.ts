3e46560e4000e86352785e7e14de03eeb4f4abb9
Александр Аргунов <argunov@nanosemantics.ai>
2020-10-13 09:24:18 +0300

    feat(localStategy, bearerStategy, mocks): remove auth

--- src/middleware/strategies/bearerStrategy.ts
+++ src/middleware/strategies/bearerStrategy.ts
@@ -1,39 +1,13 @@
 import BearerStrategy = require('passport-http-bearer')
-import axios from 'axios'
-const production = process.env.NODE_ENV === 'production'
-const API_URL = production ? process.env.STAPI_AUTH_URL : process.env.STAPI_AUTH_URL_DEV
+import { jwtTOKEN, userData } from '../mocks/user'
 
 export const bearerStrategy = new BearerStrategy(async (token, done) => {
   try {
-    const user = await axios.get(`${API_URL}users/me`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-
-    return done(null, user.data)
+    if (token === jwtTOKEN) {
+      return done(null, userData)
+    }
+    return done(null, false)
   } catch (error) {
     return done(null, false)
   }
 })
-/* 
-export const passport_authenticate_jwt = callback => {
-  const passportMiddleware = (req, res, next) => {
-    passport.authenticate('bearer', (err, user, info) => {
-      if (err) return next(err)
-      if (!user)
-        return res.status(401).send({
-          error: {
-            code: 'INVALID_AUTHORIZATION_CODE',
-            message: 'Invalid authorization code',
-          },
-        })
-
-      req.user = user
-      return callback(req, res, next)
-    })(req, res, next)
-  }
-
-  return passportMiddleware
-}
- */
--- src/middleware/strategies/localStrategy.ts
+++ src/middleware/strategies/localStrategy.ts
@@ -1,16 +1,17 @@
 import LocalStrategy = require('passport-local')
 import axios from 'axios'
+import { jwtTOKEN, localAuthUser, userData } from '../mocks/user'
 
 const production = process.env.NODE_ENV === 'production'
 const API_URL = production ? process.env.STAPI_AUTH_URL : process.env.STAPI_AUTH_URL_DEV
 
 export const localStrategy = new LocalStrategy('local', async (username, password, done) => {
   try {
-    const user = await axios.post(`${API_URL}auth/local`, {
-      identifier: username,
-      password: password,
-    })
-    return done(null, { user: user.data.user, jwt: user.data.jwt })
+    if (localAuthUser.username === username && localAuthUser.password === password) {
+      return done(null, { user: userData, jwt: jwtTOKEN })
+    }
+
+    return done(null, false)
   } catch (error) {
     console.log(error.response.headers)
     return done(null, false)

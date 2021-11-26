eeeea7fc0db75491951923aa98e9c7aa0315cd04
SOVA Deployer <deployer@sova.ai>
2021-11-26 12:16:44 +0300

    add Type ANY to default service template configs

--- src/config/serviceTemplates/serviceTemplatesConfig.ts
+++ src/config/serviceTemplates/serviceTemplatesConfig.ts
@@ -4,28 +4,28 @@ import { suitesListParams } from '../../mocks/serviceSuitesParams'
 import { profilesListParams } from '../../mocks/serviceProfilesParams'
 import { dictionariesListParams } from '../../mocks/serviceDictionariesParams'
 
-export const templatesDefaultConfig = fromJS({
+export const templatesDefaultConfig: any = fromJS({
   jsonrpc: '2.0',
   method: 'template.list',
   params: templateListParams,
   id: 1,
 })
 
-export const suitesDefaultConfig = fromJS({
+export const suitesDefaultConfig: any = fromJS({
   jsonrpc: '2.0',
   method: 'suite.list',
   params: suitesListParams,
   id: 1,
 })
 
-export const profilesDefaultConfig = fromJS({
+export const profilesDefaultConfig: any = fromJS({
   jsonrpc: '2.0',
   method: 'profile.list',
   params: profilesListParams,
   id: 1,
 })
 
-export const dictionariesDefaultConfig = fromJS({
+export const dictionariesDefaultConfig: any = fromJS({
   jsonrpc: '2.0',
   method: 'dictionary.list',
   params: dictionariesListParams,
